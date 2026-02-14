e27c0208168b65ddc022d1427638ebcba7224e6a
Baryonics <benedikt.leicher@gmail.com>
2026-02-14 17:57:45 +0100

    Restructured Vec3 into its own file to avoid circle include

---
+++ src/vec3.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+
+namespace icm20948
+{
+
+    template <typename T>
+        requires(std::integral<T> || std::floating_point<T>)
+    struct Vec3
+    {
+        Vec3() = default;
+        Vec3(T x, T y, T z)
+            : x(x)
+            , y(y)
+            , z(z)
+        {
+        }
+
+        template <typename U>
+            requires(std::integral<U> || std::floating_point<U>)
+        Vec3(const Vec3<U>& other)
+            : x(static_cast<T>(other.x))
+            , y(static_cast<T>(other.y))
+            , z(static_cast<T>(other.z))
+        {
+        }
+
+        Vec3<float> operator/(float const f) const
+        {
+            return Vec3<float>{ static_cast<float>(x) / f, static_cast<float>(y) / f, static_cast<float>(z) / f };
+        }
+
+        Vec3<T>& operator=(Vec3<T> const& v)
+        {
+            x = v.x;
+            y = v.y;
+            z = v.z;
+            return *this;
+        }
+
+        T x, y, z;
+    };
+
+} // namespace icm20948
