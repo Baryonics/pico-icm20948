b316ddc78228db47342cc26082cceb5195a11749
Baryonics <benedikt.leicher@gmail.com>
2026-02-26 21:39:23 +0100

    added more errors. Added macro because arm-gcc does not contain and_then

--- src/errors.hpp
+++ src/errors.hpp
@@ -2,12 +2,24 @@
 
 #include <cstdint>
 #include <expected>
+
+// arm-none-eabi-g++ does not contain expected::end_then() :(
+#define TRY(expr)                               \
+    do                                          \
+    {                                           \
+        if (auto _r = (expr); !_r)              \
+            return std::unexpected(_r.error()); \
+    } while (0)
+
 namespace icm20948
 {
     enum class ICMErrorT : uint8_t
     {
         i2c_write_failed,
-        i2c_read_failed
+        i2c_read_failed,
+        i2c_mag_nack,
+        ic2_mag_arb,
+        i2c_mag_timeout
     };
 
     template <typename T>
