5810b59a92fc24c7b6c15ef7266bec7cca2fd958
Baryonics <benedikt.leicher@gmail.com>
2026-03-03 13:39:33 +0100

    std::expected is a bit confusing... Used a default return type, be cautious. also overloaded []

--- src/math.hpp
+++ src/math.hpp
@@ -2,11 +2,15 @@
 
 #include "errors.hpp"
 #include <array>
+#include <cassert>
 #include <concepts>
 #include <cstdint>
 #include <expected>
+#include <iterator>
 #include <ranges>
+#include <sys/cdefs.h>
 #include <type_traits>
+#include <utility>
 
 namespace icm20948
 {
@@ -45,6 +49,17 @@ namespace icm20948
 
         template <typename U>
             requires(std::integral<U> || std::floating_point<U>)
+        auto operator*(const Vec3<U>& other) const
+        {
+            using R = std::common_type_t<T, U>;
+
+            return Vec3<R>{ static_cast<R>(x) * static_cast<R>(other.x),
+                            static_cast<R>(y) * static_cast<R>(other.y),
+                            static_cast<R>(z) * static_cast<R>(other.z) };
+        }
+
+        template <typename U>
+            requires(std::integral<U> || std::floating_point<U>)
         auto operator+(const Vec3<U>& other) const
         {
             using R = std::common_type_t<T, U>;
@@ -71,20 +86,8 @@ namespace icm20948
             return *this;
         }
 
-        auto get(const uint8_t idx) const -> ErrorT<T>
-        {
-            switch (idx)
-            {
-                case 0:
-                    return x;
-                case 1:
-                    return y;
-                case 2:
-                    return z;
-                default:
-                    return std::unexpected(ICMErrorT::out_of_bounds);
-            }
-        }
+        constexpr auto operator[](const uint8_t idx) -> T& { return (idx == 0) ? x : (idx == 1 ? y : z); }
+        constexpr auto operator[](const uint8_t idx) const -> const T& { return (idx == 0) ? x : (idx == 1 ? y : z); }
 
         T x, y, z;
     };
@@ -100,7 +103,7 @@ namespace icm20948
         {
         }
 
-        constexpr Mat3(const Vec3<T> col_1, const Vec3<T> col_2, const Vec3<T> col_3)
+        constexpr Mat3(const Vec3<T>& col_1, const Vec3<T>& col_2, const Vec3<T>& col_3)
             : m{ { { col_1.x, col_2.x, col_3.x }, { col_1.y, col_2.y, col_3.y }, { col_1.z, col_2.z, col_3.z } } }
         {
         }
@@ -116,7 +119,7 @@ namespace icm20948
             {
                 for (auto idx_c : range)
                 {
-                    out[idx_r] += static_cast<R>(m[idx_r][idx_c]) * static_cast<R>(v.get(idx_c));
+                    out[idx_r] += static_cast<R>(m[idx_r][idx_c]) * static_cast<R>(v[idx_c]);
                 }
             }
             return out;
