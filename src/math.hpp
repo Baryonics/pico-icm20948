779837274bc619c37890e7fa45d28ad43ccfa1fa
Baryonics <benedikt.leicher@gmail.com>
2026-03-17 13:15:12 +0100

    Restore math.hpp from before incorrect conflict resolution

--- src/math.hpp
+++ src/math.hpp
@@ -1,22 +1,22 @@
 #pragma once
 
-#include "errors.hpp"
 #include <array>
+#include <cassert>
 #include <concepts>
 #include <cstdint>
-#include <expected>
 #include <ranges>
+#include <sys/cdefs.h>
 #include <type_traits>
 
 namespace icm20948
 {
-
     template <typename T>
         requires(std::integral<T> || std::floating_point<T>)
     struct Vec3
     {
-        Vec3() = default;
-        Vec3(T x, T y, T z)
+        constexpr Vec3() = default;
+
+        constexpr Vec3(T x, T y, T z)
             : x(x)
             , y(y)
             , z(z)
@@ -25,26 +25,37 @@ namespace icm20948
 
         template <typename U>
             requires(std::integral<U> || std::floating_point<U>)
-        Vec3(const Vec3<U>& other)
+        constexpr Vec3(const Vec3<U>& other)
             : x(static_cast<T>(other.x))
             , y(static_cast<T>(other.y))
             , z(static_cast<T>(other.z))
         {
         }
 
-        auto operator/(float const f) const -> Vec3<float>
+        constexpr auto operator/(float const f) const -> Vec3<float>
         {
             return Vec3<float>{ static_cast<float>(x) / f, static_cast<float>(y) / f, static_cast<float>(z) / f };
         }
 
-        auto operator*(float const f) const -> Vec3<float>
+        constexpr auto operator*(float const f) const -> const Vec3<float>
         {
             return Vec3<float>{ static_cast<float>(x) * f, static_cast<float>(y) * f, static_cast<float>(z) * f };
         }
 
         template <typename U>
             requires(std::integral<U> || std::floating_point<U>)
-        auto operator+(const Vec3<U>& other) const
+        const constexpr auto operator*(const Vec3<U>& other) const
+        {
+            using R = std::common_type_t<T, U>;
+
+            return Vec3<R>{ static_cast<R>(x) * static_cast<R>(other.x),
+                            static_cast<R>(y) * static_cast<R>(other.y),
+                            static_cast<R>(z) * static_cast<R>(other.z) };
+        }
+
+        template <typename U>
+            requires(std::integral<U> || std::floating_point<U>)
+        const constexpr auto operator+(const Vec3<U>& other) const
         {
             using R = std::common_type_t<T, U>;
             return Vec3<R>{ static_cast<R>(x) + static_cast<R>(other.x),
@@ -54,7 +65,7 @@ namespace icm20948
 
         template <typename U>
             requires(std::integral<U> || std::floating_point<U>)
-        auto operator-(const Vec3<U>& other) const
+        const constexpr auto operator-(const Vec3<U>& other) const
         {
             using R = std::common_type_t<T, U>;
             return Vec3<R>{ static_cast<R>(x) - static_cast<R>(other.x),
@@ -62,27 +73,38 @@ namespace icm20948
                             static_cast<R>(z) - static_cast<R>(other.z) };
         }
 
-        auto operator=(Vec3<T> const& v) -> Vec3<T>&
+        constexpr auto operator=(const Vec3<T>& other) -> Vec3<T>&
         {
-            x = v.x;
-            y = v.y;
-            z = v.z;
+            x = other.x;
+            y = other.y;
+            z = other.z;
             return *this;
         }
 
-        auto get(const uint8_t idx) const -> ErrorT<T>
+        constexpr auto operator+=(const Vec3<T>& other) -> Vec3<T>&
         {
-            switch (idx)
-            {
-                case 0:
-                    return x;
-                case 1:
-                    return y;
-                case 2:
-                    return z;
-                default:
-                    return std::unexpected(ICMErrorT::out_of_bounds);
-            }
+            x += other.x;
+            y += other.y;
+            z += other.z;
+            return *this;
+        }
+
+        constexpr auto operator*=(const Vec3<T>& other) -> Vec3<T>&
+        {
+            x *= other.x;
+            y *= other.y;
+            z *= other.z;
+            return *this;
+        }
+
+        constexpr auto operator[](const uint8_t idx) -> T& { return (idx == 0) ? x : (idx == 1 ? y : z); }
+        constexpr auto operator[](const uint8_t idx) const -> const T& { return (idx == 0) ? x : (idx == 1 ? y : z); }
+
+        constexpr auto is_zero() -> bool
+        {
+            if (x + y + y == 0)
+                return true;
+            return false;
         }
 
         T x, y, z;
@@ -92,15 +114,15 @@ namespace icm20948
         requires(std::integral<T> || std::floating_point<T>)
     struct Mat3
     {
-        Mat3() = default;
+        constexpr Mat3() = default;
 
-        explicit Mat3(const std::array<std::array<T, 3>, 3> mat)
+        constexpr explicit Mat3(const std::array<std::array<T, 3>, 3>& mat)
             : m(mat)
         {
         }
 
-        Mat3(const Vec3<T> col_1, const Vec3<T> col_2, const Vec3<T> col_3)
-            : m{ { col_1.x, col_2.x, col_3.x }, { col_1.y, col_2.y, col_3.y }, { col_1.z, col_2.z, col_3.z } }
+        constexpr Mat3(const Vec3<T>& col_1, const Vec3<T>& col_2, const Vec3<T>& col_3)
+            : m{ { { col_1.x, col_2.x, col_3.x }, { col_1.y, col_2.y, col_3.y }, { col_1.z, col_2.z, col_3.z } } }
         {
         }
 
@@ -108,14 +130,14 @@ namespace icm20948
             requires(std::integral<U> || std::floating_point<U>)
         auto operator*(const Vec3<U>& v) const
         {
-            using R = std::common_type<T, U>;
+            using R = std::common_type_t<T, U>;
             auto range = std::views::iota(uint8_t{ 0 }, uint8_t{ 3 });
             auto out = Vec3<R>{};
             for (auto idx_r : range)
             {
                 for (auto idx_c : range)
                 {
-                    out[idx_r] += static_cast<R>(m[idx_r][idx_c]) * static_cast<R>(v.get(idx_c));
+                    out[idx_r] += static_cast<R>(m[idx_r][idx_c]) * static_cast<R>(v[idx_c]);
                 }
             }
             return out;
@@ -123,4 +145,6 @@ namespace icm20948
 
         std::array<std::array<T, 3>, 3> m{ {} }; // m[row][col]
     };
+
+    constexpr inline Mat3<float> UNIT_MAT = { Vec3<float>{ 1, 0, 0 }, Vec3<float>{ 0, 1, 0 }, Vec3<float>{ 0, 0, 1 } };
 } // namespace icm20948
